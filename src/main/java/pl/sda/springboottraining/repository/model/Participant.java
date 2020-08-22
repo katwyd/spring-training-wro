12015ae7dd6e27d5060f4701d5d158cc26b2adca
Krzysztof Gonia <krzysztof.gonia@globallogic.com>
2020-08-22 14:17:06 +0200

    spring data

--- src/main/java/pl/sda/springboottraining/repository/model/Participant.java
+++ src/main/java/pl/sda/springboottraining/repository/model/Participant.java
@@ -2,14 +2,26 @@ package pl.sda.springboottraining.repository.model;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Setter
 @Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Participant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "id_number", nullable = false, length = 10)
     private String idNumber;
 }
